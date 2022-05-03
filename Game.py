2e585e9597729032e7128f0b576380f05c00c122
amcountryman <97652506+amcountryman@users.noreply.github.com>
2022-05-03 11:00:48 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -15,7 +15,7 @@ def print_sleep(message):
 def restart_game():
     if "sword" in items:
         items.remove("sword")
-    print_sleep("GAME OVER FOR YOU\n")
+    print_sleep("GAME OVER\n")
     response = input("Would you like to play again? (y/n)")
     print_sleep("\n")
     if response == 'y':
@@ -30,11 +30,11 @@ def intro():
     print_sleep("You find yourself standing in an open area, "
                 "filled with grass and yellow wildflowers.")
     print_sleep(f"Rumor has it that a {demon} is somewhere around here, "
-                "and has been terrifying the nearby village.")
+                "and has been tormenting the nearby village.")
     print_sleep("In front of you is a house.")
     print_sleep("To your right is a dark cave.")
     print_sleep("In your hand you hold your trusty "
-                "(but not very effective) dagger.\n")
+                "(albeit small) dagger.\n")
 
 
 def field():
@@ -66,14 +66,14 @@ def house():
         print_sleep("The Sword of Ogoroth shines brightly in "
                     "your hand as you brace yourself for the attack.")
         print_sleep(f"But the {demon} takes one look at "
-                    "your shiny new toy and runs away!")
+                    "your blade and runs away!")
         print_sleep(f"You have rid the town of "
                     "the {demon}. You are victorious!")
         restart_game()
 
     elif "sword" not in items:
-        print_sleep("You feel a bit under-prepared for "
-                    "this, what with only having a tiny dagger.")
+        print_sleep("You feel woefully under-equipped for "
+                    "this, all you have is one tiny dagger.")
         response = input("Would you like to (1) fight or (2) run away?\n")
 
         if response == '1':
@@ -91,9 +91,8 @@ def house():
 def cave():
     # Things that happen to the player goes in the cave
     if "sword" in items:
-        print_sleep("You peer cautiously into the cave.Cave is full of darkness")
-        print_sleep("You've been here before, and gotten all "
-                    "the good stuff. It's just an empty cave now.")
+        print_sleep("You peer cautiously into the cave. The cave is pitch-black")
+        print_sleep("You've been here before. There is an empty scabbard in the ground. It's just an empty cave now.")
         print_sleep("You walk back out to the field.")
         field()
     else:
@@ -101,7 +100,7 @@ def cave():
         print_sleep("It turns out to be only a very small cave.")
         print_sleep("Your eye catches a glint of metal behind a rock.")
         print_sleep("You have found the magical Sword of Ogoroth!")
-        print_sleep("You discard your silly old dagger and "
+        print_sleep("You discard your dagger and "
                     "take the sword with you.")
         print_sleep("You walk back out to the field.\n")
         items.append("sword")
@@ -111,7 +110,7 @@ def woods():
     # Things that happen to the player in the woods.
     print_sleep("You approach the door of the woods.")
     print_sleep(f"You are about to knock when the "
-                "door opens and out steps a {demon}.")
+                "door swings open and out steps a {demon}.")
     print_sleep(f"Eep! This is the {demon}'s woods!")
     print_sleep(f"The {demon} attacks you!")
     # This is a conditional statement that allows the story to progress if the player has fulfilled a set condition
@@ -121,7 +120,7 @@ def woods():
         print_sleep("The Sword of Ogoroth shines brightly in "
                     "your hand as you brace yourself for the attack.")
         print_sleep(f"But the {demon} takes one look at "
-                    "your shiny new toy and runs away!")
+                    "your legendary blade and runs away!")
         print_sleep(f"You have rid the town of "
                     "the {demon}. You are victorious!")
         print_sleep("Thank you for playing this game!")
